cb1b9fda6e4fff5f19cbbc63c461283be9e77fac
maeltwosinggit <ungkuadrian@gmail.com>
2024-11-19 18:58:27 +0800

    feat: added routing capabilities (WORKING!)

--- payment-gateway-app/src/app/app.routes.ts
+++ payment-gateway-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { PaymentSummaryComponent } from './payment-summary/payment-summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'payment-form',
+        component:PaymentFormComponent
+    },
+    {
+        path:'payment-summary',
+        component:PaymentSummaryComponent
+    }
+];
---
+++ payment-gateway-app/src/app/payment-summary/payment-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-summary.component.html',
+  styleUrl: './payment-summary.component.scss'
+})
+export class PaymentSummaryComponent {
+
+}
