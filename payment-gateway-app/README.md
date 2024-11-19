8ada543db903839c75da601023810098da108608
maeltwosinggit <ungkuadrian@gmail.com>
2024-11-19 17:46:02 +0800

    Initial Project Commit

--- payment-gateway-app/README.md
+++ payment-gateway-app/README.md
@@ -1,6 +1,6 @@
-# PaymentGatewayClient
+# PaymentGatewayApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.10.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.11.
 
 ## Development server
 
--- payment-gateway-app/src/app/app.component.spec.ts
+++ payment-gateway-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'payment-gateway-client' title`, () => {
+  it(`should have the 'payment-gateway-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('payment-gateway-client');
+    expect(app.title).toEqual('payment-gateway-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, payment-gateway-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, payment-gateway-app');
   });
 });
